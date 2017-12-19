4b53e285a80982ad2f285901a13e164cd50f4917
Dwane Dunn <Dwane.Dunn@gmail.com>
2017-12-19 14:20:30 -0500

    Setup basic and global scss files

--- functions.php
+++ functions.php
@@ -86,7 +86,7 @@ add_action( 'after_setup_theme', 'campus_housing_setup' );
 /**
  * Register custom fonts.
  *
- * Borrowed from TwentySeventeen
+ * Borrowed from campus_housing
  */
 function campus_housing_fonts_url() {
 	$fonts_url = '';
@@ -123,6 +123,27 @@ function campus_housing_fonts_url() {
 
 	return esc_url_raw( $fonts_url );
 }
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function campus_housing_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'campus_housing-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'campus_housing_resource_hints', 10, 2 );
 
 
 
