3d3e185e1a91048f32f36059159a476496531170
Dwane Dunn <Dwane.Dunn@gmail.com>
2017-12-20 12:24:25 -0500

    added footer css

--- footer.php
+++ footer.php
@@ -11,21 +11,54 @@
 
 ?>
 
-	</div><!-- #content -->
+	</div> <!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'campus-housing' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'campus-housing' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'campus-housing' ), 'campus-housing', '<a href="https://dwanedunn.com">Dwane Dunn</a>' );
-			?>
-		</div><!-- .site-info -->
+    <?php get_sidebar( 'footer' ); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+        <div class="site-footer__wrap">
+
+        <!-- Make sure there is a social menu to display.-->
+            <?php if ( has_nav_menu( 'social' ) ) { ?>
+
+                <nav class="social-menu">
+
+                    <?php
+                    wp_nav_menu( array(
+
+                        'theme_location'    => 'menu-2',
+                        'menu_class'        => 'social-links-menu',
+                        'depth'             => 1,
+                        'link_before'       => '<span class="screen-reader-text">',
+                        'link_after'        => '</span>',
+                        // 'link_after'        => '</span>' . humescores_get_svg( array( 'icon' => 'chain' ) ),
+
+                    ) );
+                    ?>
+
+                </nav> <!-- .social-menu -->
+
+            <?php } ?> <!-- Endif has_nav_menu( 'social' )  -->
+
+            <div class="site-info">
+
+                <div>
+                    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'campus-housing' ) ); ?>"> <?php printf( esc_html__( 'Proudly powered by %s', 'campus-housing' ), 'WordPress' ); ?>
+                    </a>
+                </div>
+
+                <div>
+                    <?php
+
+                        printf( esc_html__( 'Theme: %1$s by %2$s.', 'campus-housing' ), 'campus-housing', '<a href="https://dwanedunn.com" rel="designer" >Dwane Dunn</a>' );
+                    ?>
+                </div>
+
+            </div> <!-- .site-info -->
+        </div> <!-- .site-footer__wrap -->
 	</footer><!-- #colophon -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
