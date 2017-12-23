97721c29971fb337d87c721efd4cd536d76d173b
Dwane Dunn <Dwane.Dunn@gmail.com>
2017-12-23 16:14:20 -0500

    Setup customizer for header and footer color changes.

--- inc/customizer.php
+++ inc/customizer.php
@@ -24,6 +24,7 @@ function campus_housing_customize_register( $wp_customize ) {
 			'selector'        => '.site-description',
 			'render_callback' => 'campus_housing_customize_partial_blogdescription',
 		) );
+
 	}
 
 	// Setting for header and footer background color
@@ -75,6 +76,68 @@ function campus_housing_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function campus_housing_customize_preview_js() {
-	wp_enqueue_script( 'campus-housing-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'campus-housing-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), time(), true );
 }
 add_action( 'customize_preview_init', 'campus_housing_customize_preview_js' );
+
+
+
+
+
+
+if ( ! function_exists( 'campus_housing_header_style' ) ) :
+	/**
+	 * Styles the header image and text displayed on the blog.
+	 *
+	 * @see campus_housing_custom_header_setup().
+	 */
+	function campus_housing_header_style() {
+		$header_text_color = get_header_textcolor();
+		$header_bg_color = get_theme_mod( 'theme_bg_color' );
+
+		/*
+		 * If no custom options for text are set, let's bail.
+		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+		 */
+		if ( get_theme_support( 'custom-header', 'default-text-color' ) != $header_text_color ) {
+
+
+			// If we get this far, we have custom styles. Let's do this.
+			?>
+			<style type="text/css">
+				<?php
+				// Has the text been hidden?
+				if ( ! display_header_text() ) :
+				?>
+				.site-title,
+				.site-description {
+					position: absolute;
+					clip: rect(1px, 1px, 1px, 1px);
+				}
+				<?php
+					// If the user has set a custom color for the text use that.
+					else :
+				?>
+				.site-title a,
+				.site-description {
+					color: #<?php echo esc_attr( $header_text_color ); ?>;
+				}
+				<?php endif; ?>
+			</style>
+			<?php
+
+		}
+
+
+		if ( '#002254' != $header_bg_color ) { ?>
+			<style type="text/css">
+				.site-header,
+				.site-footer {
+					background-color: <?php echo esc_attr( $header_bg_color ); ?>;
+				}
+			</style>
+		<?php
+		}
+
+	}
+endif;
